dd5232db83030ec6f12ad7b00f0be3fdc3fa9122
xiaomu <hunt_hak@outlook.com>
2020-11-16 21:27:32 +0800

    update

--- Leetcode/1116/mergeTwoList.py
+++ Leetcode/1116/mergeTwoList.py
@@ -1,7 +1,19 @@
 class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+    def __init__(self, item):
+        self.item = item
+        self.next = None
+
+    # def __iter__(self):
+    #     return self
+
+
+"""
+链表失败了！！！
+优化方法：
+插值排序，快速排序等
+
+查找法，二分查找法
+"""
 
 
 class Solution:
@@ -16,7 +28,33 @@ class Solution:
         elif l2 == []:
             return l1
         else:
-            head = ListNode(l1[0])
+            if len(l1) < len(l2):
+                node_list = self.create_linklist_tail(l1)
+                tmp_list = l2
+            else:
+                node_list = self.create_linklist_tail(l2)
+                tmp_list = l1
+
+            # print(node_list)
+
+            while node_list:
+                num = node_list.item
+                tmp = node_list  # 保存前一个值
+                for index2, j in enumerate(tmp_list):
+                    if num < j:
+                        tmp = node_list
+                        node_list = node_list.next
+                        if not node_list:
+                            node_list = tmp
+                            node_list.next = j
+                        else:
+                            break
+                    else:
+                        node = ListNode(j)
+                        node_list.next = node
+                        tmp.next = node_list.item
+                        tmp_list.pop(0)
+            return node_list
 
     def create_linklist_tail(self, nums):
         if len(nums) > 1:
@@ -31,3 +69,8 @@ class Solution:
             return ListNode(nums[0])
         else:
             return []
+
+
+solu = Solution()
+res = solu.mergeTwoLists(l1=[1, 2, 4], l2=[1, 3, 4])
+print(res)
